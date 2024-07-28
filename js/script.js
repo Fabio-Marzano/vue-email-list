61de58f6fd44d860dc12a6e3d3aebfeb1c19c9e1
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-28 11:28:14 +0200

    [UPD] generate-first-email

--- js/script.js
+++ js/script.js
@@ -3,12 +3,17 @@ const { createApp} = Vue;
 createApp({
     data() {
         return {
-            emails: null
+            emails: []
         }
+    },
+    created() {
+        this.getRandomEmails();
     },
     methods: {
         getRandomEmails() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => (
+                this.emails.push(result.data.response)
+            ));
         }
     }
 }).mount('#app');
