d4f44def09ef9ca0db79a5ed93975858d826d7ed
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-28 12:26:16 +0200

    [UPD] generate-10-emails-random

--- js/script.js
+++ js/script.js
@@ -11,9 +11,12 @@ createApp({
     },
     methods: {
         getRandomEmails() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => (
-                this.emails.push(result.data.response)
-            ));
+            for(let i = 0; i<10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => (
+                    this.emails.push(result.data.response)
+                ));
+
+            }
         }
     }
 }).mount('#app');
