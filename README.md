db2845a30e774e91f67fc1164851417867ca037d
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-28 12:34:49 +0200

    [UPD] create-file-readme

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 SOLUZIONE
-1-
+1- Creo un array per inserire l'email da generare e attivo la funzione di return.
+2- Genero l'email all'interno del DOM 
+3- Avvio un ciclo for per generare l'inserimento delle 10 emails random.
+4- Utilizzo l'API di Boolean per mostrare l'email. 
+5- Confronto il risultato dell'email create utilizzando Thunder Client. 
+6- Pusho l'email all'interno del DOM.
+7- Mostro il risultato finale.
--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 const { createApp} = Vue;
-
+//CREO L'ARRAY PER INSERIRE LE EMAIL DA GENERARE//
 createApp({
     data() {
         return {
             emails: []
         }
     },
+    //GENERO L'EMAIL ALL'INTERNO DEL DOM//
     created() {
         this.getRandomEmails();
     },
     methods: {
+        //AVVIO CICLO FOR PER GENERARE L'INSERIMENTO DELLE 10 EMAILS RANDOM//
         getRandomEmails() {
             for(let i = 0; i<10; i++) {
+                //UTILIZZO L'API DI BOOLEAN PER MOSTRARE LE EMAIL//
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => (
+                    //PUSHO L'EMAIL ALL'INTERNO DEL DOM//
                     this.emails.push(result.data.response)
                 ));
 
